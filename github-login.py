40f480a4bc8b0147c37011f0d95523736ac3f012
amirhossein bigdelu <amirbig44@gmail.com>
2022-06-01 22:16:54 +0430

    Add files via upload

---
+++ github-login.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+session = requests.Session()
+
+url = 'https://github.com/{}'
+username = 'amirbigg'
+
+r = session.get(url.format('login'))
+content = BeautifulSoup(r.text, 'html.parser')
+
+data = {}
+for form in content.find_all('form'):
+	for inp in form.select('input[type=hidden]'):
+		data[inp.get('name')] = inp.get('value')
+
+data.update({'login':'amirbig44@gmail.com', 'password':'amirhossein44'})
+
+r = session.post(url.format('session'), data=data)
+r = session.get(url.format(username))
+content = BeautifulSoup(r.text, 'html.parser')
+user_info = content.find(class_='vcard-details')
+print(user_info.text)
